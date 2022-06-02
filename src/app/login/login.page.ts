9598a7359a44ad1871d5e000c6106ac10ea2d4eb
juanchehin <chehinjuan@gmail.com>
2022-06-02 12:13:01 -0300

    login service + page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth-service.service';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +12,13 @@ export class LoginPage implements OnInit {
 
   isLoading = false;
   isLogin = true;
+  router: any;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
+  ) { }
 
   ngOnInit() {
   }
@@ -18,13 +26,45 @@ export class LoginPage implements OnInit {
   authenticate(email: string, password: string) {
     this.isLoading = true;
 
-    // Chequear con user y pass de drive
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: 'Logging in...' })
+      .then(loadingEl => {
+        loadingEl.present();
+        let authObs: Observable<any>;
+        if (this.isLogin) {
+          authObs = this.authService.login(email, password);
+        } else {
+          this.showAlert('Error de logueo');
+          // authObs = this.authService.signup(email, password);
+        }
+        authObs.subscribe(
+          resData => {
+            this.isLoading = false;
+            loadingEl.dismiss();
+            this.router.navigateByUrl("/personal");
+          },
+          errRes => {
+            loadingEl.dismiss();
+            const code = errRes.error.error.message;
+            let message = 'Could not sign you up, please try again.';
+            if (code === 'EMAIL_EXISTS') {
+              message = 'This email address exists already!';
+            } else if (code === 'EMAIL_NOT_FOUND') {
+              message = 'E-Mail address could not be found.';
+            } else if (code === 'INVALID_PASSWORD') {
+              message = 'This password is not correct.';
+            }
+            this.showAlert(message);
+          }
+        );
+      });
+
+
   }
 
   onSwitchAuthMode() {
     this.isLogin = !this.isLogin;
   }
-
 
   private showAlert(message: string) {
     this.alertCtrl
---
+++ src/app/services/auth-service.service.ts
@@ -0,0 +1,160 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, from } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { environment } from '../../environments/environment.prod';
+
+// import { Plugins } from '@capacitor/core';
+// import { Storage } from '@capacitor/Storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private _user = new BehaviorSubject<User>(null);
+  private activeLogoutTimer: any;
+
+  get userIsAuthenticated() {
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return !!user.token;
+        } else {
+          return false;
+        }
+      })
+    );
+  }
+
+  get userId() {
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return user.id;
+        } else {
+          return null;
+        }
+      })
+    );
+  }
+
+  get token() {
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return user.token;
+        } else {
+          return null;
+        }
+      })
+    );
+  }
+
+  constructor(private http: HttpClient) {}
+
+  autoLogin() {
+    return from(Storage.get({ key: 'authData' })).pipe(
+      map((storedData: any) => {
+        if (!storedData || !storedData.value) {
+          return null;
+        }
+        const parsedData = JSON.parse(storedData.value) as {
+          token: string;
+          tokenExpirationDate: string;
+          userId: string;
+          email: string;
+        };
+        const expirationTime = new Date(parsedData.tokenExpirationDate);
+        if (expirationTime <= new Date()) {
+          return null;
+        }
+        const user = new User(
+          parsedData.userId,
+          parsedData.email,
+          parsedData.token,
+          expirationTime
+        );
+        return user;
+      }),
+      tap(user => {
+        if (user) {
+          this._user.next(user);
+          this.autoLogout(user.tokenDuration);
+        }
+      }),
+      map(user => {
+        return !!user;
+      })
+    );
+  }
+
+
+  login(email: string, password: string) {
+    // Chequear con user y pass de drive
+    if(email === environment.user && password == environment.pass)
+    {
+      return true;
+    }
+    return false;
+  }
+
+  logout() {
+    if (this.activeLogoutTimer) {
+      clearTimeout(this.activeLogoutTimer);
+    }
+    this._user.next(null);
+    Storage.remove({ key: 'authData' });
+  }
+
+  ngOnDestroy() {
+    if (this.activeLogoutTimer) {
+      clearTimeout(this.activeLogoutTimer);
+    }
+  }
+
+  private autoLogout(duration: number) {
+    if (this.activeLogoutTimer) {
+      clearTimeout(this.activeLogoutTimer);
+    }
+    this.activeLogoutTimer = setTimeout(() => {
+      this.logout();
+    }, duration);
+  }
+
+  // Guarda los datos del usuario en caso de que este autenticado
+  private setUserData(userData: AuthResponseData) {
+    const expirationTime = new Date(
+      new Date().getTime() + +userData.expiresIn * 1000
+    );
+    const user = new User(
+      userData.localId,
+      userData.email,
+      userData.idToken,
+      expirationTime
+    );
+    this._user.next(user);
+    this.autoLogout(user.tokenDuration);
+    this.storeAuthData(
+      userData.localId,
+      userData.idToken,
+      expirationTime.toISOString(),
+      userData.email
+    );
+  }
+
+  private storeAuthData(
+    userId: string,
+    token: string,
+    tokenExpirationDate: string,
+    email: string
+  ) {
+    const data = JSON.stringify({
+      userId: userId,
+      token: token,
+      tokenExpirationDate: tokenExpirationDate,
+      email: email
+    });
+    Storage.set({ key: 'authData', value: data });
+  }
+}
