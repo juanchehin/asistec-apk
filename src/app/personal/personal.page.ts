c0573696121291de5fea373b452b76c0cd10723c
chehin <chehinjuan@gmail.com>
2022-06-05 20:20:06 -0300

    listado personal

--- src/app/personal/personal.page.ts
+++ src/app/personal/personal.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Personal } from '../models/personal.model';
+import { PersonalService } from '../services/personal.service';
 
 @Component({
   selector: 'app-personal',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalPage implements OnInit {
 
-  constructor() { }
+  personal: any;
+  desde = 0;
+
+  constructor(
+    private personalService: PersonalService
+  ) { }
 
   ngOnInit() {
+    this.cargarPersonal();
+  }
+
+  cargarPersonal(){
+
+    this.personalService.cargarPersonal( this.desde )
+             .subscribe( (resp: any) => {
+              console.log('resp es : ', resp);
+
+              this.personal = resp[0];
+
+
+            });
   }
 
 }
---
+++ src/app/services/personal.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
+
+const URL_SERVICIOS = environment.URL_SERVICIOS;
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonalService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  cargarPersonal(desde){
+
+    desde = desde || 0;
+
+    let url = URL_SERVICIOS + '/personal/listar/' + desde;
+
+    return this.http.get(url);
+  }
+}
