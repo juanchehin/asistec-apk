05165c5bc9de8f79fc017a739e498ac0e8ecd1e2
chehin <chehinjuan@gmail.com>
2022-06-06 15:52:48 -0300

    escuelas

---
+++ apk/src/app/escuelas/escuelas.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-escuelas',
+  templateUrl: './escuelas.page.html'
+})
+export class EscuelasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
